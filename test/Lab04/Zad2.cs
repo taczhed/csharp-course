81d3a3a3e3456ecd53f05db35ee314a4dc57dd32
Patryk Lach <patryk.lach@mp.pl>
2023-12-02 16:08:30 +0100

    feat(Lab04): exceptions exercise

---
+++ test/Lab04/Zad2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+
+class Lab04Zad2
+{
+    public static double TrianglePerimeter(int a, int b, int c, int precision = 2)
+    {
+        if (a < 0 || b < 0 || b < 0 || precision < 2 || precision > 8) throw new ArgumentOutOfRangeException("wrong arguments");
+        if (a + b < c || a + c < b || b + c < a) throw new ArgumentException("object not exist");
+
+        return a + b + c;
+    }
+
+
+    static void Zad(string[] args)
+    {
+        Console.Write("a:");
+        var a = int.Parse(Console.ReadLine());
+
+        Console.Write("b:");
+        var b = int.Parse(Console.ReadLine());
+
+        Console.Write("c:");
+        var c = int.Parse(Console.ReadLine());
+
+        Console.Write("precision:");
+        var precision = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine(TrianglePerimeter(a, b, c, precision));
+    }
+
+}
