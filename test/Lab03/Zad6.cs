d56ad3be1873cc689987533289ffd0d229954808
Patryk Lach <patryk.lach@mp.pl>
2023-12-02 10:24:15 +0100

    feat(Lab03): cross exercise

---
+++ test/Lab03/Zad6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+class Lab3Zad6
+{
+    public static void Wzorek(int n)
+    {
+        if (n < 3) return;
+
+        var size = n % 2 == 0 ? n - 1 : n;
+        for (var y = 0; y < n; y++)
+        {
+            for (var x = 0; x < size; x++)
+            {
+                if (x == y || x == size - y - 1) Console.Write('*');
+                else Console.Write(' ');
+            }
+            Console.Write('\n');
+        }
+    }
+
+    static void Zad(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Wzorek(n);
+    }
+
+}
--- test/Program.cs
+++ test/Program.cs
@@ -1,54 +1,12 @@
 ﻿using System;
 using System.Globalization;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        string wejscie = Console.ReadLine();
-        short[] dane = Array.ConvertAll<string, short>(wejscie.Split(" "), short.Parse);
-        short a = dane[0], b = dane[1], c = dane[2];
-
-        if (dane[1] > dane[0])
-        {
-            a = dane[0];
-            b = dane[1];
-        }
-        else
-        {
-            a = dane[1];
-            b = dane[0];
-        }
-
-        string str = "";
-        for (int i = a + 1; i < b; i++)
-        {
-            if (i % c == 0)
-            {
-                str += $"{i}";
-                str += "_";
-            }
-        }
-
-        if (a - b == 0 || str == "")
-        {
-            Console.WriteLine("empty");
-            return;
-        }
-
-        str = str.Remove(str.Length - 1);
-
-        string[] listOfShorts = str.Split("_");
-
-        if (listOfShorts.Length <= 10)
-        {
-            Console.Write(String.Join(", ", listOfShorts));
-            return;
-        }
-
-        string[] result = { listOfShorts[0].ToString(), listOfShorts[1].ToString(), listOfShorts[2].ToString(), "...", listOfShorts[listOfShorts.Length - 2].ToString(), listOfShorts[listOfShorts.Length - 1].ToString() };
-
-        Console.Write(String.Join(", ", result));
+        Console.WriteLine("csharp-course");
     }
 
 }
