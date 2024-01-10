d9eee3dab6beb385fdfaf7fa3e3fa8d8e19a1226
Patryk Lach <patryk.lach@mp.pl>
2024-01-10 18:06:08 +0100

    feat(Lab07): exercises done

---
+++ test/Lab07/Classes/Child.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Lab07.Classes
+{
+    public class Child : Person
+    {
+        private Person mother_ref;
+        private Person father_ref;
+
+        public Child(string firstName, string familyName, int age, Person mother = null, Person father = null)
+            : base(firstName, familyName, age)
+        {
+            mother_ref = mother;
+            father_ref = father;
+
+            ValidateChildAge(age);
+        }
+
+        public override string ToString()
+        {
+            string motherInfo = (mother_ref != null) ? $"mother: {mother_ref}" : "mother: No data";
+            string fatherInfo = (father_ref != null) ? $"father: {father_ref}" : "father: No data";
+
+            return $"{FirstName} {FamilyName} ({Age})\n{motherInfo}\n{fatherInfo}";
+        }
+
+
+        public new void modifyAge(int age)
+        {
+            ValidateChildAge(age);
+
+            if (age < 0 || age > 15)
+            {
+                throw new ArgumentException("Child’s age must be between 0 and 15!");
+            }
+
+            base.modifyAge(age);
+        }
+
+        private void ValidateChildAge(int age)
+        {
+            if (age > 15)
+            {
+                throw new ArgumentException("Child’s age must be less than 15!");
+            }
+        }
+    }
+}
---
+++ test/Lab07/Classes/Person.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Lab07.Classes
+{
+    public class Person
+    {
+        public string firstName;
+        public string familyName;
+        public int age;
+
+        public Person(string firstName, string familyName, int age)
+        {
+            FirstName = firstName;
+            FamilyName = familyName;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {FamilyName} ({Age})";
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            private set => firstName = FormatName(value);
+            
+        }
+
+        public string FamilyName
+        {
+            get => familyName;
+            private set => familyName = FormatName(value);
+        }
+
+        public int Age
+        {
+            get => age;
+            private set => age = FormatAge(value);
+        }
+
+        public void modifyFirstName(string name)
+        {
+            firstName = FormatName(name);
+        }
+
+        public void modifyFamilyName(string name)
+        {
+            familyName = FormatName(name);
+        }
+
+        public void modifyAge(int _age)
+        {
+            age = FormatAge(_age);
+        }
+
+        private string FormatName(string name)
+        {
+            name = name.Replace(" ", "");
+
+            char[] tempName = new char[name.Length];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!char.IsLetter(name[i]))
+                {
+                    throw new ArgumentException("Wrong name!");
+                }
+
+                if (i == 0)
+                {
+                    tempName[0] = char.ToUpper(name[0]);
+                    continue;
+                }
+
+                tempName[i] = char.ToLower(name[i]);
+            }
+
+            return new string(tempName);
+        }
+
+        private int FormatAge(int age)
+        {
+            if (age < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+
+            return age;
+        }
+
+    }
+}
